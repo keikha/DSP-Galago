64fd115a28116876acced92f75df690c7fb9a07d
Mostafa Keikha <keikha@gmail.com>
2014-06-10 15:17:41 -0400

    bigram queries
    modified SDM query generator to generate only bigram queries

--- src/ciir/umass/edu/sum/Hierarchy.java
+++ src/ciir/umass/edu/sum/Hierarchy.java
@@ -222,7 +222,7 @@ public class Hierarchy {
 	public void estimate(String query, int topD) throws Exception
 	{
 		//System.out.println(QueryProcessor.generateSDMFieldQuery(query, field));
-        ScoredDocument[] r = se.runQuery(QueryProcessor.generateSDMFieldQuery(query, field), topD);
+        ScoredDocument[] r = se.runQuery(QueryProcessor.generateBigramFieldQuery(query, field), topD);
 		//ScoredDocument[] r = se.runQuery(generateSDMFieldQuery(query), topD);
 		//ScoredExtentResult[] r = se.runQuery("#1(" + query + ")", topD);
 		
